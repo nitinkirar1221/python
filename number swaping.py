47d886203db70eccc9938cd9c7382114168a4a61
nitinkirar1221 <nitinkirar8982@gmail.com>
2025-10-13 12:57:27 +0530

    Add files via upload

---
+++ number swaping.py
@@ -0,0 +1,11 @@
+a = int (input("enter the number ="))
+b = int (input("enter the number ="))
+c = int (input("enter the number ="))
+
+
+print("a =",a ,"b =",b,"c=",c)
+
+a,b,c = c,b,a 
+
+print("swapped")
+print("a =",a,"b =",b,"c=",c) 
